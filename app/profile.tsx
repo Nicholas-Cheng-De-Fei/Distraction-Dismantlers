9fd0df4ac3a342173c103f841536fcf5f17be8ea
Nicholas Cheng <nicholas_cheng@u.nus.edu>
2024-06-30 03:44:42 +0800

    Fixed profile page

--- app/profile.tsx
+++ app/profile.tsx
@@ -5,12 +5,13 @@ import { signOut } from "@firebase/auth";
 import { doc, collection, getDocs, query, where, getDoc, updateDoc, DocumentData } from "firebase/firestore";
 import { useIsFocused } from "@react-navigation/native";
 import { styles, width, height } from "@/assets/style";
+import Tasks from "@/components/Tasks";
 
 export default function Profile() {
 
   const isFocused = useIsFocused();
-  const currentUserId = auth.currentUser!.uid;
-  
+  const user = auth!.currentUser;
+
   const yesterdayDate = new Date();
   yesterdayDate.setDate(yesterdayDate.getDate() - 1);
 
@@ -18,7 +19,7 @@ export default function Profile() {
   todayDate.setHours(23, 59, 59);
 
   const startOfWeekDate = new Date();
-  startOfWeekDate.setDate( startOfWeekDate.getDate() - startOfWeekDate.getDay() + 1);
+  startOfWeekDate.setDate(startOfWeekDate.getDate() - startOfWeekDate.getDay() + 1);
   startOfWeekDate.setHours(0, 0, 0);
 
 
@@ -30,26 +31,26 @@ export default function Profile() {
   const [average, setAverage] = React.useState(0);
   const [streakCount, setStreakCount] = React.useState(0);
 
-  function resetStreak (currentData : DocumentData, currentUserId: string) {
+  function resetStreak(currentData: DocumentData, currentUserId: string) {
     updateDoc(doc(database, "streak", currentUserId), {
-      Days : 0,
-      lastStudied : currentData.lastStudied,
-    }).then(() => {console.log("Streak Reset")});
+      Days: 0,
+      lastStudied: currentData.lastStudied,
+    }).then(() => { console.log("Streak Reset") });
   }
-  
-  async function getUserStats (currentUserId : string) {
-  
+
+  async function getUserStats(currentUserId: string) {
+
     const statsRef = collection(database, "stats", currentUserId, "studySessions");
-    const queryStats = query(statsRef,  where("Date", "<=", todayDate), where("Date", ">=", startOfWeekDate));
+    const queryStats = query(statsRef, where("Date", "<=", todayDate), where("Date", ">=", startOfWeekDate));
     const streakRef = doc(database, "streak", currentUserId);
-  
-    let count : number = 0;
-  
+
+    let count: number = 0;
+
     try {
       const streakDataSnap = await getDoc(streakRef);
       const streakData = streakDataSnap.data();
-  
-      const time = new Date(streakData!.lastStudied.toDate().getTime()  + 8 * 60 * 60 * 1000);
+
+      const time = new Date(streakData!.lastStudied.toDate().getTime() + 8 * 60 * 60 * 1000);
       if (time.toDateString() == yesterdayDate.toDateString() || time.toDateString() == new Date().toDateString()) {
         count = streakData!.Days;
       } else {
@@ -58,81 +59,85 @@ export default function Profile() {
           resetStreak(streakData!, currentUserId);
         }
       }
-  
+
       setStreakCount(count);
-  
-      let avg : number = 0;
-  
+
+      let avg: number = 0;
+
       const sessionsDataSnap = await getDocs(queryStats);
       sessionsDataSnap.forEach((doc) => {
-        avg += doc.data().Duration/60/60;
+        console.log(doc);
+        avg += doc.data().Duration / 60 / 60;
       });
-  
-      avg /= todayDate.getDay();
-  
+
+      avg /= Math.max(todayDate.getDay(), 1);
+
       setAverage(avg);
-  
+
     } catch (error) {
       console.log(error);
     }
-  
+
   }
 
   React.useEffect(() => {
     if (isFocused) {
-      getUserStats(currentUserId);
+      if (user?.uid != null) {
+        getUserStats(user!.uid);
+      }
     }
   }, [isFocused])
 
   return (
-    <View style = {styles.background}>
-      <View style={{ justifyContent: "center", alignItems: "center", paddingTop: 80 }}>
-        <Button title="Logout" onPress={logout} color="#e74c3c" />
+    <View style={styles.background}>
+      <View style={styles.ProfileHeader}>
+        <Text style={styles.ProfileHeaderText}>Hello {user!.displayName}</Text>
       </View>
 
-      <View style = {{flexDirection: 'row'}}>
+      <View style={{ flexDirection: 'row' }}>
 
-        <View id="streak" style = {{flex : 1}}>
-          <View style = {styles.streakBoxHeader}>
-            <Text style = {{fontSize : 20, fontWeight: 'bold'}}>Streak</Text>
+        <View id="streak" style={{ flex: 1 }}>
+          <View style={styles.streakBoxHeader}>
+            <Text style={{ fontSize: 20, fontWeight: 'bold' }}>Streak</Text>
           </View>
-          <View style = {styles.streakInfoBox}>
-            <Image 
+          <View style={styles.streakInfoBox}>
+            <Image
               source={require('@/assets/images/Profile-icon/streak.png')}
-              style = {{
+              style={{
                 width: 55,
                 height: 55,
               }}
-              ></Image>
-              <Text style = {{fontSize: 27}}>{streakCount} Days</Text>
+            ></Image>
+            <Text style={{ fontSize: 27 }}>{streakCount} Days</Text>
           </View>
         </View>
 
-        <View id = "average time" style = {{flex : 1}}>
-          <View style = {styles.focusBoxHeader}>
-            <Text style = {{fontSize : 20, fontWeight: 'bold'}}>Avg Focus Time</Text>
+        <View id="average time" style={{ flex: 1 }}>
+          <View style={styles.focusBoxHeader}>
+            <Text style={{ fontSize: 20, fontWeight: 'bold' }}>Avg Focus Time</Text>
           </View>
-          <View style = {styles.focusInfoBox}>
-          <Image 
+          <View style={styles.focusInfoBox}>
+            <Image
               source={require('@/assets/images/Profile-icon/time.png')}
-              style = {{
+              style={{
                 width: 55,
                 height: 55,
               }}
-              ></Image>
-              <Text style = {{fontSize: 27}}>{average.toFixed(2)} hrs/wk</Text>
+            ></Image>
+            <Text style={{ fontSize: 27 }}>{average.toFixed(2)} hrs/wk</Text>
           </View>
         </View>
+      </View>
+      <View id="to-do List">
+        <Tasks />
+      </View>
 
-        <View id = "to-do List">
+      <View id="heatMap">
 
-        </View>
+      </View>
 
-        <View id = "heatMap">
-          
-        </View>
-
-
+      <View style={{ justifyContent: "center", alignItems: "center", paddingTop: 80 }}>
+        <Button title="Logout" onPress={logout} color="#e74c3c" />
       </View>
     </View>
   );
