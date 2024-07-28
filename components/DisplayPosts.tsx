4a61521d4afaf5dc5d902b626d88d63a7988cd43
Nicholas Cheng <nicholas_cheng@u.nus.edu>
2024-07-28 16:58:36 +0800

    Added a feature allowing users to see a list of their own posts

--- components/DisplayPosts.tsx
+++ components/DisplayPosts.tsx
@@ -5,7 +5,7 @@ import { width, height, styles } from "@/assets/style";
 import { doc, getDoc, getDocs, query, collection, where, orderBy } from '@firebase/firestore';
 import ThreadSearch from "./ThreadSearch";
 
-async function getUserSubcribedCourses(currentUserId: string, setData: any) {
+async function getUserSubcribedCourses(currentUserId: string, setCommunityPost: any, setPersonalPost: any, setRenderData : any) {
     if (currentUserId != undefined) {
         try {
             const followedCoursesRef = doc(database, "subscriptions", currentUserId);
@@ -15,14 +15,28 @@ async function getUserSubcribedCourses(currentUserId: string, setData: any) {
             const q = query(collection(database, "thread"), where("course", "in", followedCoursesData!.courses), orderBy("datePosted", "desc"));
             const querySnapshot = await getDocs(q);
 
-            let posts = [];
+            let postsCommunity = [];
 
             querySnapshot.forEach((doc) => {
                 // doc.data() is never undefined for query doc snapshots
-                posts.push(doc);
+                postsCommunity.push(doc);
             });
 
-            setData(posts);
+            setCommunityPost(postsCommunity);
+
+            const q2 = query(collection(database, "thread"), where("uid", "==", currentUserId), orderBy("datePosted", "desc"));
+            const querySnapshot2 = await getDocs(q2);
+
+            let postsPersonal= [];
+
+            querySnapshot2.forEach((doc) => {
+                // doc.data() is never undefined for query doc snapshots
+                postsPersonal.push(doc);
+            });
+
+            setPersonalPost(postsPersonal);
+
+            setRenderData(postsCommunity);
 
         } catch (error) {
             console.log(error);
@@ -43,11 +57,18 @@ function changePannel(setPannel: any, setMod: any, setPost: any, pannel: string,
 export default function DisplayPosts({ setPannel, setMod, setPost }: any) {
 
     const user = auth!.currentUser;
-    const [courseData, setData] = React.useState([]);
+    const [communityPosts, setCommunityPost] = React.useState([]);
+    const [personalPost, setPersonalPost] = React.useState([]);
+
+    const [renderData, setRenderData] = React.useState([]);
+
     const [canRender, setCanRender] = React.useState(false);
 
+    const [communityFocus, setCommunityFocus] = React.useState(true);
+    const [personalFocus, setPersonalFocus] = React.useState(false);
+
     React.useEffect(() => {
-        getUserSubcribedCourses(user!.uid, setData).then(() => { setCanRender(true) });
+        getUserSubcribedCourses(user!.uid, setCommunityPost, setPersonalPost, setRenderData).then(() => {setCanRender(true) });
     }, []);
 
     return (
@@ -59,16 +80,28 @@ export default function DisplayPosts({ setPannel, setMod, setPost }: any) {
                         <View>
                             <ThreadSearch setPannel={setPannel} setMod={setMod} />
                         </View>
+                        <View style = {{top: height * 0.085, flexDirection : "row" ,justifyContent : "space-evenly"}}>
+                            <Pressable onPress={() => {setCommunityFocus(true); setPersonalFocus(false); setRenderData(communityPosts)}}>
+                                <View style = {{backgroundColor : "white", borderRadius : 10, width : width * 0.3, alignItems : "center"}}>
+                                    <Text style = {{fontSize : 20, color: communityFocus ? '#FF7F7F' : '#8e918f', fontWeight: communityFocus ? 'bold' : 'normal'}}>Community</Text>
+                                </View>
+                            </Pressable>
+                            <Pressable onPress={() => {setCommunityFocus(false); setPersonalFocus(true), setRenderData(personalPost)}}>
+                                <View style = {{backgroundColor : "white", borderRadius : 10, width : width * 0.3, alignItems : "center"}}>
+                                    <Text style = {{fontSize : 20, color: personalFocus ? '#FF7F7F' : '#8e918f', fontWeight: personalFocus ? 'bold' : 'normal'}}>Your Post</Text>
+                                </View>
+                            </Pressable>
+                        </View>
                         <View style={{ top: height * 0.1 }}>
                             {
-                                courseData.length == 0
+                                renderData.length == 0
                                     ? <View style={{ alignItems: "center" }}>
                                         <Image source={require("@/assets/images/empty-list-icon.png")} style={{ width: 400, height: 400 }}></Image>
                                         <Text style={{ fontSize: 24, fontWeight: "bold", flexWrap: 'wrap', textAlign: "center" }}>Start following courses and see them appear here!</Text>
                                     </View>
                                     : <View style = {{height : height * 0.75}}>
                                         <FlatList
-                                            data={courseData}
+                                            data={renderData}
                                             showsHorizontalScrollIndicator={false}
                                             bounces={false}
                                             renderItem={(item) => {
